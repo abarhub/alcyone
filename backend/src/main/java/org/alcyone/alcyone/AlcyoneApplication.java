5ee1dc5867c2ac312c91c310e1d193602c137723
abarhub <abarhub@gmail.com>
2026-06-20 13:02:36 +0200

    commit initial

---
+++ backend/src/main/java/org/alcyone/alcyone/AlcyoneApplication.java
@@ -0,0 +1,13 @@
+package org.alcyone.alcyone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlcyoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlcyoneApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/org/alcyone/alcyone/AlcyoneApplicationTests.java
@@ -0,0 +1,13 @@
+package org.alcyone.alcyone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlcyoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
